c43767ade93faee726b4140e3093da1b1c8c7643
biswaranjan420 <55794791+biswaranjan420@users.noreply.github.com>
2024-05-01 17:48:50 +0530

    test;

---
+++ JenkingTest.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class JenkingTest {
+	
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		LocalTime time  = LocalTime.now();
+		
+		System.out.println("Jenking Working for Java App");
+		System.out.println("Date :" + date);
+		System.out.println("Time :" + time);
+	}
+
+}
